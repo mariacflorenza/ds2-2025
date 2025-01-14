c547ee3f8e22c98500814512ab3175264c6b4e12
Guillaume Maze <gmaze@ifremer.fr>
2025-01-14 14:42:34 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -71,4 +71,7 @@ def check_up_env(with_tuto=False):
             urllib.request.urlretrieve(os.path.join(repo, "practice/exploratory_statistics/tuto_tools.py"),
                                        os.path.join(".", "tuto_tools.py"))
             import tuto_tools as ds2tools
-            return ds2tools
+        return ds2tools
+    else:
+        return None
+
